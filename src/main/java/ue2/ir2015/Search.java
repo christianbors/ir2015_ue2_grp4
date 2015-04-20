6ec08935a4c2315da51796e9613104538fdad6ad
Christian Bors <christianbors@gmail.com>
2015-04-20 11:30:15 +0200

    initial project commit including search example

---
+++ src/main/java/ue2/ir2015/Search.java
@@ -0,0 +1,78 @@
+package ue2.ir2015;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.*;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by christianbors on 16/04/15.
+ */
+public class Search {
+    public static void main(String[] args) throws IOException, ParseException {
+        Analyzer analyzer = new StandardAnalyzer();
+
+        Directory index = new RAMDirectory();
+        List<File> queue = new ArrayList<File>();
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+
+        IndexWriter iwriter = new IndexWriter(index, config);
+        addDoc(iwriter, "Lucene in Action", "193398817");
+        addDoc(iwriter, "Lucene for Dummies", "55320055Z");
+        addDoc(iwriter, "Managing Gigabytes", "55063554A");
+        addDoc(iwriter, "The Art of Computer Science", "9900333X");
+        iwriter.close();
+        String querystr = args.length > 0 ? args[0] : "lucene";
+
+        Query q = null;
+        try {
+            q = new QueryParser("title", analyzer).parse(querystr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        // 3. search
+        int hitsPerPage = 10;
+        IndexReader reader = DirectoryReader.open(index);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage);
+        searcher.search(q, collector);
+        ScoreDoc[] hits = collector.topDocs().scoreDocs;
+
+        // 4. display results
+        System.out.println("Found " + hits.length + " hits.");
+        for (int i = 0; i < hits.length; ++i) {
+            int docId = hits[i].doc;
+            Document d = searcher.doc(docId);
+            System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
+        }
+
+        // reader can only be closed when there
+        // is no need to access the documents any more.
+        reader.close();
+    }
+
+    private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
+        Document doc = new Document();
+        doc.add(new TextField("title", title, Field.Store.YES));
+
+        // use a string field for isbn because we don't want it tokenized
+        doc.add(new StringField("isbn", isbn, Field.Store.YES));
+        w.addDocument(doc);
+    }
+}
